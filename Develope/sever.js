9df02cfa3bbd39cd3e6b8fac4f845639c2b36187
Rojel Perez Vez <rojelpv@gmail.com>
2020-05-07 18:24:04 -0400

    adding server.js to connect to mongodb

---
+++ Develope/sever.js
@@ -0,0 +1,31 @@
+// Dependencies
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+// Setting up Express App
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(morgan("dev"));
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// db mongo
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
+
+// Creating Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Starts the server to begin listening
+app.listen(PORT, function () {
+    console.log(`App listening on Port ${PORT}!`);
+});
