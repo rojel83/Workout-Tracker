bbce44b385e0b433ef5ca0de5ffafbc7d50dd9a7
Rojel Perez Vez <rojelpv@gmail.com>
2020-05-07 18:48:59 -0400

    deplying wit mongodb

--- Develope/sever.js
+++ Develope/sever.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // db mongo
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useFindAndModify: false
